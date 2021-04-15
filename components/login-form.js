3e72dbcd12157b7a2c85c99cccb3e2cf26304f04
nebiyu kifle <rema9.ne@gmail.com>
2021-04-14 21:45:54 -0700

    just startes

---
+++ components/login-form.js
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+
+export default function LoginForm({ onSubmit }) {
+
+    const initialValues = {
+        username: '',
+        password: '',
+    }
+
+    const [values, setValues] = useState(initialValues);
+
+    function submitHandler(event) {
+        event.preventDefault();
+        onSubmit(values);
+        setValues(initialValues)
+    }
+
+    function inputChangeHandler(event) {
+
+        let { name, value } = event.target;
+
+        setValues({ ...values, [name]: value });
+    }
+
+    return (
+
+        <form onSubmit={submitHandler}>
+            <div className="flex flex-col h-auto bg-green-200">
+            <div >
+                <label htmlFor="username">User Name</label>
+                <input type="text" name="username" id="username" value={values.username} onChange={inputChangeHandler} placeholder="User Name" />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" value={values.password} onChange={inputChangeHandler} placeholder="password" />
+            </div>
+            <div>
+            <button type="submit">Sign In</button>
+            </div>
+            </div>
+        </form>
+    );
+}
+
+
+
+// import { useState } from 'react'
+
+
+// export default function LoginForm({ onSubmit }){
+
+//     const initialValues = {
+//         username: '',
+//         password: '',
+//     }
+
+//     const [values, setValues] = useState(initialValues);
+
+//     function submitHandler(event) {
+//         event.preventDefault();
+//         onSubmit(values);
+//         setValues(initialValues)
+//     }
+
+//     function inputChangeHandler(event) {
+
+//         let { name, value } = event.target;
+
+//         setValues({ ...values, [name]: value });
+//     }
+
+//     return (
+
+//         <form onSubmit={submitHandler}>
+//             <div>
+//                 <label htmlFor="username">User Name</label>
+//                 <input type="text" name="username" id="username" value={values.username} onChange={inputChangeHandler} placeholder="User Name" />
+//             </div>
+
+//             <div>
+//                 <label htmlFor="password">Password</label>
+//                 <input type="password" name="password" id="password" value={values.password} onChange={inputChangeHandler} placeholder="password" />
+//             </div>
+
+//             <button type="submit">Sign In</button>
+
+//         </form>
+//     );
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,167 +1,210 @@
-import Head from 'next/head'
-import Link from 'next/link'
-
-import { hours } from '../data'
 import { useState } from 'react'
+import CookieStandAdmin from '../components/cookie-stand-admin';
+import LoginForm  from '../components/login-form.js';
+import { getToken } from '../services/data-fetcher'
 
 
 export default function Home() {
 
-  const hourlySales = [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
-  const [cookieData, setcookieData] = useState([])
+    const [token, setToken] = useState();
 
-  // const [location, setLocation] = useState('locations')
-  // const [minCustomer, setMinCustomer] = useState('minCustomers')
-  // const [maxCustomer, setMaxCustomer] = useState('maxCustomers')
-  // const [avgCookies, setAvgCookies] = useState('avgCookiess')
-  
-  function formHandler(event){
-    event.preventDefault();
-    const newCookieData = {
+    const [username, setUsername] = useState('');
 
-       setLocation: event.target.location.value,
-       setMinCustomer: event.target.minCustomer.value,
-       setMaxCustomer: event.target.maxCustomer.value,
-       setAvgCookies: event.target.avgCookies.value,
-       hourlySales: hourlySales,
-       length: cookieData.length
+    async function loginHandler(values) {
+
+        const fetchedToken = await getToken(values);
+
+        setToken(fetchedToken);
+
+        setUsername(values.username);
     }
-    setcookieData([...cookieData, newCookieData])
 
-  }
+    function logoutHandler() {
+        setToken(null);
+    }
+
+    if (!token) return <LoginForm onSubmit={loginHandler} />
+
+    return <h2><CookieStandAdmin token={token} onLogout={logoutHandler} username={username} /></h2>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Head from 'next/head'
+// import Link from 'next/link'
+
+// import { hours } from '../data'
+// import { useState } from 'react'
+
+
+// export default function Home() {
+
+//   const hourlySales = [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
+//   const [cookieData, setcookieData] = useState([])
+
+//   // const [location, setLocation] = useState('locations')
+//   // const [minCustomer, setMinCustomer] = useState('minCustomers')
+//   // const [maxCustomer, setMaxCustomer] = useState('maxCustomers')
+//   // const [avgCookies, setAvgCookies] = useState('avgCookiess')
+  
+//   function formHandler(event){
+//     event.preventDefault();
+//     const newCookieData = {
+
+//        setLocation: event.target.location.value,
+//        setMinCustomer: event.target.minCustomer.value,
+//        setMaxCustomer: event.target.maxCustomer.value,
+//        setAvgCookies: event.target.avgCookies.value,
+//        hourlySales: hourlySales,
+//        length: cookieData.length
+//     }
+//     setcookieData([...cookieData, newCookieData])
+
+//   }
 
   
-  return (
-    <div className="min-h-screen py-5 bg-green-50">
-      <Head>
-        <title>Cookie-stand-admin</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+//   return (
+//     <div className="min-h-screen py-5 bg-green-50">
+//       <Head>
+//         <title>Cookie-stand-admin</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <main className="">
-        <Header /> 
-        <SubTitle />
-        <Form/>
-        <br/>
+//       <main className="">
+//         <Header /> 
+//         <SubTitle />
+//         <Form/>
+//         <br/>
 
-        <br className=""></br>
-        <div className="pt-4 bg-green-200">
+//         <br className=""></br>
+//         <div className="pt-4 bg-green-200">
       
-        <Table data={cookieData}/>
-        </div>
-        <Footer />
+//         <Table data={cookieData} location={cookieData.location}/>
+//         </div>
+//         <Footer />
         
-      </main>
-    </div>
-  )
+//       </main>
+//     </div>
+//   )
 
-  function Header(props){
-    return(
-      <header className="pt-4 text-black bg-green-500 border">
-        <h1 className="text-4xl">Cookie Stand Admin</h1>
+//   function Header(props){
+//     return(
+//       <header className="pt-4 text-black bg-green-500 border">
+//         <h1 className="text-4xl">Cookie Stand Admin</h1>
         
-        <button className="float-right px-2 py-1 bg-gray-200 rounded -m-9" >
-          <Link href="/overview">
-          <a className="">Overview</a>
-          </Link>
-        </button>
-      </header>
-    )
-  }
+//         <button className="float-right px-2 py-1 bg-gray-200 rounded -m-9" >
+//           <Link href="/overview">
+//           <a className="">Overview</a>
+//           </Link>
+//         </button>
+//       </header>
+//     )
+//   }
 
 
-  function SubTitle(props){
-    return(
-      <div className="my-3 bg-green-200 w-100 h-400">
-        <h2 className="pt-3 font-bold text-center">Create Cooke Stand</h2>
-      </div>
-    )
-  }
+//   function SubTitle(props){
+//     return(
+//       <div className="my-3 bg-green-200 w-100 h-400">
+//         <h2 className="pt-3 font-bold text-center">Create Cooke Stand</h2>
+//       </div>
+//     )
+//   }
 
-  function Form(props){
-    return(
-      <form onSubmit={formHandler} className="w-9/12 p-4 m-auto my-4 bg-green-300 rounded">
-        <div className="flex flex-row">
+//   function Form(props){
+//     return(
+//       <form onSubmit={formHandler} className="w-9/12 p-4 m-auto my-4 bg-green-300 rounded">
+//         <div className="flex flex-row">
         
-        <div>
-        <label className="mb-4 text-2xl text-center">Location</label>
-        </div>
+//         <div>
+//         <label className="mb-4 text-2xl text-center">Location</label>
+//         </div>
         
-        <div>
-        <input name="location" className="flex-1 text-black"></input>
-        <button className="flex flex-row"></button>
-        </div>
-        <br/>
+//         <div>
+//         <input name="location" className="flex-1 text-black"></input>
+//         <button className="flex flex-row"></button>
+//         </div>
+//         <br/>
         
 
-        <div flex items-end gap-x-3 pt-8>
-        <label>Minimum Customers per Hour</label>
-        <input name="minCustomer"className="pl-1"></input>
-        </div>
+//         <div flex items-end gap-x-3 pt-8>
+//         <label>Minimum Customers per Hour</label>
+//         <input name="minCustomer"className="pl-1"></input>
+//         </div>
         
-        <div>
-        <label>Maximum Customers per Hour</label>
-        <input name="maxCustomer" className="pl-1"></input>
-        </div>
+//         <div>
+//         <label>Maximum Customers per Hour</label>
+//         <input name="maxCustomer" className="pl-1"></input>
+//         </div>
 
-        <div>
-        <label>Average Cookies per Sale</label>
-        <input name="avgCookies" className="pl-1"></input>
-        </div>
-        <br/>
+//         <div>
+//         <label>Average Cookies per Sale</label>
+//         <input name="avgCookies" className="pl-1"></input>
+//         </div>
+//         <br/>
 
-        <div className="my-4 ">
-        <button type="submit" className="mt-8 bg-green-500 text-gray-50" >Create</button>
-        </div > 
+//         <div className="my-4 ">
+//         <button type="submit" className="mt-8 bg-green-500 text-gray-50" >Create</button>
+//         </div > 
 
-        </div>
-      </form>
-    )
-  }
+//         </div>
+//       </form>
+//     )
+//   }
   
-  function Table(props){
+//   function Table(props){
 
-    return(
-      <div className="mb-10 text-center">
-      <table className="w-9/12 m-auto">
-      <thead>
-        <tr>
-          <th>location </th>{hours.map(hour =>(<th>{hour}</th>))}
-          <th>Total</th>
-        </tr>
-      </thead>
+//     return(
+//       <div className="mb-10 text-center">
+//       <table className="w-9/12 m-auto">
+//       <thead>
+//         <tr>
+//           <th>location </th>{hours.map(hour =>(<th>{hour}</th>))}
+//           <th>Total</th>
+//         </tr>
+//       </thead>
 
-      <tbody className="">
-        {props.data.map(cookieData =>(
-          <tr className="odd:bg-green-400">
-            <td className="border border-gray-400">{cookieData.location}</td>
+//       <tbody className="">
+//         {props.data.map(cookieData =>(
+//           <tr className="odd:bg-green-400">
+//             <td className="border border-gray-400">{cookieData.location}</td>
 
-            {cookieData.hourlySales.map(total =>(<td>{total}
-            </td>))}
-            <td>{cookieData.hourlySales.reduce((a, b)=> a + b, 0)}</td>
-          </tr>
-        ))}
+//             {cookieData.hourlySales.map(total =>(<td>{total}
+//             </td>))}
+//             <td>{cookieData.hourlySales.reduce((a, b)=> a + b, 0)}</td>
+//           </tr>
+//         ))}
 
-      </tbody>
+//       </tbody>
 
-      <tfoot>
-        <tr>
-          <th>Total</th>
-        </tr>
-      </tfoot>
-    </table>
-    </div>
-  )
-}
+//       <tfoot>
+//         <tr>
+//           <th>Total</th>
+//         </tr>
+//       </tfoot>
+//     </table>
+//     </div>
+//   )
+// }
 
-function Footer(props){
-  return(
-    <footer className="mt-8 bg-green-500 text-gray-50">
-    <p className="text-black ">&copy; 2021 {cookieData.length}Locations World Wide</p>
-  </footer>
-  )        
-  }
-}
+// function Footer(props){
+//   return(
+//     <footer className="mt-8 bg-green-500 text-gray-50">
+//     <p className="text-black ">&copy; 2021 {cookieData.length}Locations World Wide</p>
+//   </footer>
+//   )        
+//   }
+// }
 
 
 
